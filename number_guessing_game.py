00a6e0c30d46d7e32876069f2ad033114ec9abf0
swaraj_AI_engineer <swarajshejulcloud@gmail.com>
2026-03-10 14:31:40 +0530

    updated variable name in number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,7 +2,7 @@
 import getpass # this module allows us to hide the input of the user, so that the other player cannot see the number that is being entered.
 
 number_of_players = int(input("Enter the number of players more than one : "))
-count= []
+players= []
 selected_numbers = {}
 
 # this loop is used to get the names of the players and store them in a list called count. 
@@ -12,22 +12,22 @@ selected_numbers = {}
 
 for i in range(number_of_players):
     name = input(f"Enter player{i+1} name: ")
-    count.append(name)
+    players.append(name)
 
 
-print("Welcome to the number guessing game, " + ", ".join(count) + "!")
+print("Welcome to the number guessing game, " + ", ".join(players) + "!")
 
 # this loop is used to get the selected numbers from each player and store them in the selected_numbers dictionary. 
 # The getpass.getpass() function is used to hide the input of the user, so that the other player cannot see the number that is being entered.
 
-for i in count:
+for i in players:
     number = int(getpass.getpass(i +"Enter number between 1 and 20: "))
     selected_numbers[i] = number
 
 # this loop is used to allow each player to take turns guessing the numbers selected by the other players.
 
 while True:
-    for i in count:
+    for i in players:
         player_turn = int(input(f"{i}, guess a number :"))
         if player_turn in selected_numbers.values():
             keys = [k for k, v in selected_numbers.items() if v == player_turn]
